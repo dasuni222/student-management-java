28e89e671d28b78d3b8664f75f718c486c4222ce
Dasuni Navodya <dasunirajapaksha2002@gmail.com>
2026-06-13 17:08:55 +0530

    Make view student method

--- src/StudentService.java
+++ src/StudentService.java
@@ -6,5 +6,14 @@ public class StudentService {
         students.add(student);
         System.out.println("Student added successfully");
     }
-    
+
+    public void viewStudent(){
+       if (students.isEmpty()){
+            System.out.println("No students found");
+            return;
+       }
+       for (Student s : students){
+        System.out.println(s.getId() + " | " + s.getName() + " | " + s.getCourse() ); 
+       }    
+    }
 }
