4b69c1e7bef4d6cf24915e9b10611651996a8731
Dasuni Navodya <dasunirajapaksha2002@gmail.com>
2026-06-13 23:01:17 +0530

    Make the Main class with Scanner for user includes

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        StudentService service = new StudentService();
+        int choice;
+        
+    }
+}
