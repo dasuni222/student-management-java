11e167f85be07680f2a0f24832143dd29d8d35d2
Dasuni Navodya <dasunirajapaksha2002@gmail.com>
2026-06-17 22:44:06 +0530

    Updated student class

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,4 @@
-public class Student{
+public class Student {
     private int id;
     private String name;
     private String course;
@@ -6,26 +6,10 @@ public class Student{
     public Student(int id, String name, String course) {
         this.id = id;
         this.name = name;
-        this.course = course;   
-    }
-
-    public int getId(){
-        return id;
-    }
-    public String getName(){
-        return name;
-    }
-    public String getCourse(){
-        return course;
-    }
-    public void setId(int id){
-        this.id = id;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setCourse(String course){
         this.course = course;
     }
 
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getCourse() { return course; }
 }
