a6c817cd3345a6444b2677f7541eb39ea487dc9b
Dasuni Navodya <dasunirajapaksha2002@gmail.com>
2026-06-18 23:34:10 +0530

    Updated the main class for fix errors

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,10 @@
 import java.util.Scanner;
-public class Main{
-    public static void main (String[] args){
+
+public class Main {
+    public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         StudentService service = new StudentService();
-        int choice;
-     
-        do{
-           System.out.println("\n1. Add Student");
-           System.out.println("View Students");
-           System.out.println("Search Student");
-           System.out.println("Delete Students");
-           System.out.println("Exit");
 
-           choice = input.nextInt();
-           switch(choice){
-            case 1:
-                System.out.print("ID: ");
-                int id = input.nextInt();
-
-                input.nextLine();
-
-                System.out.print("Name: ");
-                String name = input.nextLine();
-
-                System.out.print("Course: ");
-                String course = input.nextLine();
-
-                Student student = new Student(id, name, course);
-
-                service.addStudent(student);
-                break;
-
-            case 2 :
-                service.viewStudent();
-                break;
-
-            case 3 :
-                System.out.print("Enter Id : ");
-                int searchId = input.nextInt();
-                service.searchStudent(searchId);
-                break;
-
-            case 4 :
-                System.out.print("Enter Id : ");
-                int deleteId = input.nextInt();
-                service.deleteStudent(deleteId);
-                break;
-
-            case 5 :
-                System.out.print("Program Closed.");
-                break;
-
-            default :
-                System.out.println("Invalid choice.");
-
-           }
-        } while(choice != 5);
-    }
+    }    
 }
