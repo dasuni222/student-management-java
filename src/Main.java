b7dbc7c6f330c0ea7b1984e26a65660e1da1c2da
Dasuni Navodya <dasunirajapaksha2002@gmail.com>
2026-06-19 22:35:47 +0530

    Create the do while loop

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,18 @@ public class Main {
         Scanner input = new Scanner(System.in);
         StudentService service = new StudentService();
 
+          do {
+            System.out.println("\n===== STUDENT MENU =====");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Update Student");
+            System.out.println("6. Exit");
+            System.out.print("Enter choice: ");
+
+            choice = input.nextInt();
+            
+          }    
     }    
 }
